7c768170a1c9aa6a1cc0e4c8c5407ae8046bdfca
jhilee <juhuilee01@gmail.com>
2019-10-31 22:47:51 -0700

    graph on the same page

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -16,23 +16,22 @@ class RankingsController < ApplicationController
 
   def _ranking
     @keyword = Keyword.find(params[:id])
-    @rankings = Ranking.where(keyword_id:params[:id])
-    @google_ranks = Ranking.extract_data(@rankings,"google")
-    @google_base_ranks = Ranking.extract_data(@rankings,"google_base")
-    @bing_ranks = Ranking.extract_data(@rankings,"bing")
-    @yahoo_ranks = Ranking.extract_data(@rankings,"yahoo")
-    @gsv = Ranking.extract_data(@rankings,"gsv")
-    @google_wranks = Ranking.get_weighted_rank(@keyword.id,@google_ranks,@gsv)
-    @google_base_wranks = Ranking.get_weighted_rank(@keyword.id,@google_base_ranks,@gsv)
-    @bing_wranks = Ranking.get_weighted_rank(@keyword.id,@bing_ranks,@gsv)
-    @yahoo_wranks = Ranking.get_weighted_rank(@keyword.id,@yahoo_ranks,@gsv)
+
+    respond_to do |format|
+      format.html {redirect_to ranking_url}
+      format.js {render :layout => false}
+    end
+
+
   end
 
   def export_csv
     respond_to do |format|
       selected_rankings = Ranking.where(keyword_id: params["keyword_id"], date_ranked: [params["from"]..params["to"]])
       format.html
-      format.csv { send_data selected_rankings.to_csv(), filename: "Ranking Data-#{params["keyword"]}-#{Date.today}.csv" }
+      format.csv {
+        send_data selected_rankings.to_csv(), type: 'text/csv', filename: "Ranking Data-#{params["keyword"]}-#{Date.today}.csv"
+      }
     end
   end
 end
